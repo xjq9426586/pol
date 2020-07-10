e4b9c61fe08516895ac1f7da0e7474e51ec23006
Administrator <931408673@qq.com>
2020-07-10 17:15:11 +0800

    分组后不改变顺序

--- src/main/java/java8/stream/MapFilter.java
+++ src/main/java/java8/stream/MapFilter.java
@@ -19,46 +19,54 @@ public class MapFilter {
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
     public static void main(String[] args) {
-        try {
-           List<String> c = Files.lines(Paths.get("D:\\work0\\credit_190718\\财报导航配置修改.patch"))
-                    .filter(line -> line.startsWith("Index:"))
-                   .collect(Collectors.toList());
-            c.forEach(System.out::println);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-//        List list = new ArrayList();
-//        Map<String, Object> HOSTING = new HashMap<>();
-//        HOSTING.put("1", "linode.com");
-//        HOSTING.put("2", "heroku.com");
-//        HOSTING.put("3", "digitalocean.com");
-//        HOSTING.put("4", "aws.amazon.com");
-//
-//        //Map -> Stream -> Filter -> Map
-//        Map<String, Object> result1 = HOSTING.entrySet().stream()
-//                .filter(map -> map.getKey().equals(""))
-//                .collect(Collectors.toMap(h -> h.getKey(), h -> h.getValue()));
-//
-//        System.out.println(result1);
-//
-//
-//        Map<String, Object> HOSTING1 = new HashMap<>();
-//        HOSTING1.put("11", "linode.com");
-//        HOSTING1.put("2", "heroku.com");
-//        HOSTING1.put("3", "digitalocean.com");
-//        HOSTING1.put("4", "aws.amazon.com");
-//        list.add(HOSTING);
-//        list.add(HOSTING1);
-//        Map<String, Object> result2 = HOSTING1.entrySet().stream()
-//                .filter(map -> map.getKey().equals(""))
-//                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-//        System.out.println(result2);
-//
-//        System.out.println(filterByKey(HOSTING1, x -> x.equals("11")));
-//        System.out.println(HOSTING1.containsValue("digitalocean.com1"));
+
+        List list = new ArrayList();
+        Map<String, Object> HOSTING = new HashMap<>();
+        HOSTING.put("1", "linode.com");
+        HOSTING.put("2", "heroku.com");
+        HOSTING.put("3", "digitalocean.com");
+        HOSTING.put("4", "aws.amazon.com");
+
+        //Map -> Stream -> Filter -> Map
+        Map<String, Object> result1 = HOSTING.entrySet().stream()
+                .filter(map -> map.getKey().equals(""))
+                .collect(Collectors.toMap(h -> h.getKey(), h -> h.getValue()));
+
+        System.out.println(result1);
 
 
+        Map<String, Object> HOSTING1 = new HashMap<>();
+        HOSTING1.put("11", "linode.com");
+        HOSTING1.put("2", "heroku.com");
+        HOSTING1.put("3", "digitalocean.com");
+        HOSTING1.put("4", "aws.amazon.com");
+        list.add(HOSTING);
+        list.add(HOSTING1);
+        Map<String, Object> result2 = HOSTING1.entrySet().stream()
+                .filter(map -> map.getKey().equals(""))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        System.out.println(result2);
+
+        System.out.println(filterByKey(HOSTING1, x -> x.equals("11")));
+        System.out.println(HOSTING1.containsValue("digitalocean.com1"));
+
+
+        List<Map<String, Object>> list1 = new ArrayList<>();
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("sn", 1);
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("sn", 1);
+        Map<String, Object> map3 = new HashMap<>();
+        map3.put("sn", 1);
+        list1.add(map2);
+        list1.add(map1);
+        list1.add(map3);
+        Comparator<Map<String, Object>> comparator = Comparator.comparing(m -> ((Integer) m.get("sn")));
+        list1.sort(comparator.reversed());
+        //list1.sort(comparator);
+        //list1.stream().sorted(Comparator.comparing(m -> ((Integer) m.get("sn")))).collect(Collectors.toList());
+
+        System.out.println(list1);
     }
 
 }
--- src/main/java/java8/stream/MapGroupTest.java
+++ src/main/java/java8/stream/MapGroupTest.java
@@ -14,18 +14,33 @@ public class MapGroupTest {
         map.put("a", 123);
         map.put("v", 345);
         map.put("n", "n");
-        map.put("d", "2020");
+        map.put("d", 1);
         Map<String, Object> map1 = new HashMap<>();
         map1.put("a", 123);
         map1.put("v", 2);
         map1.put("n", "n");
-        map1.put("d", "2018");
-        List<Map<String, Object>> list = Arrays.asList(map1, map);
+        map1.put("d", 1);
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("a", 12);
+        map2.put("v", 2);
+        map2.put("n", "n");
+        map2.put("d", 2);
+
+        Map<String, Object> map4 = new HashMap<>();
+        map4.put("a", 12);
+        map4.put("v", 2);
+        map4.put("n", "n");
+        map4.put("d", 2);
+        List<Map<String, Object>> list = Arrays.asList(map, map1, map2, map4);
         List newList = list.stream().filter(m -> m.get("d").equals("2019")).collect(Collectors.toList());
-        System.out.println(newList);
+        //System.out.println(newList);
 
+        Comparator<Map<String, Object>> comparator = Comparator.comparing(m -> ((Integer) m.get("d")));
+        list.sort(comparator);
+        //分组后不改变顺序
+        LinkedHashMap m = list.stream().collect(Collectors.groupingBy(e -> e.get("a"), LinkedHashMap::new, Collectors.toList()));
 
-        Map m = list.stream().collect(Collectors.groupingBy(e -> e.get("a")));
+        //Map m = list.stream().collect(Collectors.groupingBy(e -> e.get("a")));
         List a = new ArrayList();
         m.forEach((k, v) -> {
             Map<String, Object> re = new HashMap<>();
